0c03134787cc5b93a920c87f1228a39d5db38ec3
Alex <mordocks24@gmail.com>
2026-06-02 23:49:47 -0500

    finished move logic

--- pieces.py
+++ pieces.py
@@ -6,21 +6,47 @@ class Piece:
         self.team = team
     def get_valid_moves(self, board):
         return
-    def attempt_move(self, board, input):
-        return
+    def attempt_move(self, board, targeted_coord_string):
+        target_row, target_column = translate_coords(targeted_coord_string)
+        current_row, current_col = translate_coords(self.position)
+
+        if (target_row, target_column) in self.get_valid_moves(board):         
+            board[target_row][target_col].resident = self
+            self.position = targeted_coord_string
+            board[current_row][current_col].resident = None
+            return True
+        else:
+            print("Illegal Move!")
+            return False
 
 class Pawn(Piece):
     def __init__(self, position, team):
         super().__init__(position, team)
         self.symbol = "♟" if team == "white" else "♙"
     
-    def get_valid_moves(self, position):
-        if self.team == "white":
-            forward_once = 1
-        else:
-            forward_once = -1
+    def get_valid_moves(self, board):
+        current_row, current_col = translate_coords(self.position)
+        valid_moves = []
+        direction = 1 if self.team == "white" else -1
+        target_row = current_row + direction
 
-
+        # moves forward once 
+        if 0 <= target_row <= 7:
+            if board[target_row][current_col].resident is None:
+                valid_moves.append((target_row, (current_col)))
+        # moves forward twice
+                start_row = 1 if self.team == "white" else 6
+                if current_row == start_row:
+                    double_target = current_row + (direction * 2)
+                    if board[double_target][current_col].resident is None:
+                        valid_moves.append((double_target, current_col))
+        #capture diagonally
+        for cap in [-1, 1]:
+            target_column = current_col + cap
+            if 0 <= target_row <= 7 and 0 <= target_column <= 7:
+                diagonal_square = board[target_row][target_column]
+                if diagonal_square.resident != None and diagonal_square.resident.team != self.team:
+                    valid_moves.append((target_row, target_column))
 
 class Rook(Piece):
     def __init__(self, position, team):
