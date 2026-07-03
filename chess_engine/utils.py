b471cd0c0f97f4916b65853ca6ffff935eebde66
Alex <mordocks24@gmail.com>
2026-07-03 14:37:00 -0500

    d

--- chess_engine/utils.py
+++ chess_engine/utils.py
@@ -1,6 +1,13 @@
 
 file_map = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
 
+def translate_indices(row, col):
+    # Convert row index back to rank (8-0 -> '1'-'8')
+    # Convert col index back to file (0-7 -> 'a'-'h')
+    letter = chr(ord('a') + col)
+    number = str(8 - row)
+    return letter + number
+
 def translate_coords(coord_string):
     if len(coord_string) != 2:
         raise ValueError("Invalid format, 2 characters please.")
@@ -46,18 +53,22 @@ def is_checkmate(team, board):
                 valid_moves = piece.get_valid_moves(board)
                 
                 for move_coords in valid_moves:
-
                     tr, tc = move_coords
                     cr, cc = translate_coords(original_pos)
-                    
 
+                    # 1. Save state
                     original_target_resident = board[tr][tc].resident
+                    
+                    # 2. Simulate move
                     board[tr][tc].resident = piece
                     board[cr][cc].resident = None
-                    piece.position = "e4"
+                    
+                    # FIX: Set the piece's position to the actual destination string
+                    piece.position = translate_indices(tr, tc) 
 
                     still_in_check = is_in_check(team, board)
 
+                    # 3. Undo move (Restore state)
                     board[cr][cc].resident = piece
                     board[tr][tc].resident = original_target_resident
                     piece.position = original_pos
